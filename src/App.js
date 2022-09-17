be5641aebde9ad512bd0fd7ea0e90a3ef2a464f8
sharipoff-0-1 <tedcodes.01@gmail.com>
2022-09-17 10:15:28 +0200

    form

--- src/App.js
+++ src/App.js
@@ -4,30 +4,41 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      num: 1,
+      title: "",
     };
   }
 
   render() {
-    const plus = () => {
-      this.setState({ num: this.state.num + 1 });
+    // const plus = () => {
+    //   this.setState({ num: this.state.num + 1 });
+    // };
+
+    // const minus = () => {
+    //   if (this.state.num > 0) {
+    //     this.setState({ num: this.state.num - 1 });
+    //   }
+    // };
+
+    const getEmail = (e) => {
+      this.setState({ title: e.target.value });
     };
 
-    const minus = () => {
-      if (this.state.num > 0) {
-        this.setState({ num: this.state.num - 1 });
-      }
+    const getPassword = (e) => {
+      this.setState({ pass: e.target.value });
     };
 
     return (
-      <div className="counter">
-        <button className="btn" onClick={plus}>
-          +
-        </button>
-        <p className="count">{this.state.num}</p>
-        <button className="btn" onClick={minus}>
-          -
-        </button>
+      <div className="container">
+        <h2>Create an account</h2>
+        <div className="form">
+          <label htmlFor="email">Email</label>
+          <input type="email" onChange={getEmail} />
+          <label htmlFor="password">Password</label>
+          <input type="password" onChange={getPassword} />
+          <button className="btn">Create an account</button>
+        </div>
+        <p className="emailValue">User Email: {this.state.title}</p>
+        <p className="passwordValue">User Password: {this.state.pass}</p>
       </div>
     );
   }
